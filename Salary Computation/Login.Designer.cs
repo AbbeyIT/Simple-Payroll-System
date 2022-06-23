6ee3a8a105ff580c8fb451620a7b3a50ab602118
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-06-23 15:32:09 +0800

    add login form

---
+++ Salary Computation/Login.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Salary_Computation
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.radioButtonSalaryComp = new System.Windows.Forms.RadioButton();
+            this.checkBoxShowHide = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.Font = new System.Drawing.Font("Raleway SemiBold", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button1.Location = new System.Drawing.Point(202, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkKhaki;
+            this.label3.Font = new System.Drawing.Font("Raleway Black", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(-1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(507, 66);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Login Area";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.textBoxUsername.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.Location = new System.Drawing.Point(202, 99);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(168, 27);
+            this.textBoxUsername.TabIndex = 4;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.textBoxPassword.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(202, 143);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(168, 27);
+            this.textBoxPassword.TabIndex = 5;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // radioButtonSalaryComp
+            // 
+            this.radioButtonSalaryComp.AutoSize = true;
+            this.radioButtonSalaryComp.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonSalaryComp.Font = new System.Drawing.Font("Poppins SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonSalaryComp.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.radioButtonSalaryComp.Location = new System.Drawing.Point(202, 203);
+            this.radioButtonSalaryComp.Name = "radioButtonSalaryComp";
+            this.radioButtonSalaryComp.Size = new System.Drawing.Size(124, 27);
+            this.radioButtonSalaryComp.TabIndex = 7;
+            this.radioButtonSalaryComp.TabStop = true;
+            this.radioButtonSalaryComp.Text = "Salary System";
+            this.radioButtonSalaryComp.UseVisualStyleBackColor = false;
+            this.radioButtonSalaryComp.CheckedChanged += new System.EventHandler(this.radioButtonSalaryComp_CheckedChanged);
+            // 
+            // checkBoxShowHide
+            // 
+            this.checkBoxShowHide.AutoSize = true;
+            this.checkBoxShowHide.BackColor = System.Drawing.Color.GhostWhite;
+            this.checkBoxShowHide.Location = new System.Drawing.Point(387, 149);
+            this.checkBoxShowHide.Name = "checkBoxShowHide";
+            this.checkBoxShowHide.Size = new System.Drawing.Size(53, 17);
+            this.checkBoxShowHide.TabIndex = 8;
+            this.checkBoxShowHide.Text = "Show";
+            this.checkBoxShowHide.UseVisualStyleBackColor = false;
+            this.checkBoxShowHide.CheckedChanged += new System.EventHandler(this.checkBoxShowHide_CheckedChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(506, 329);
+            this.Controls.Add(this.checkBoxShowHide);
+            this.Controls.Add(this.radioButtonSalaryComp);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogIn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.RadioButton radioButtonSalaryComp;
+        private System.Windows.Forms.CheckBox checkBoxShowHide;
+    }
+}
---
+++ Salary Computation/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Salary_Computation
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            if (username == "IsabelIT" && password == "ilovecsharp00")
+            {
+                if (radioButtonSalaryComp.Checked)
+                {
+                    PayrollSystem salaryComp = new PayrollSystem();
+                    salaryComp.Show();
+                    this.Hide();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect username or password, Please try again.");
+            }
+           
+        }
+
+        private void checkBoxShowHide_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxShowHide.Checked)
+            {
+                textBoxPassword.UseSystemPasswordChar = false;
+            }    
+            else
+            {
+                textBoxPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void radioButtonSalaryComp_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
