dff5dea0e34aab2e491f78abf04e0a41e28a2bc7
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-06-23 15:32:41 +0800

    add deduction class

---
+++ Salary Computation/Deductions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_Computation
+{
+    class Deductions
+    {
+        private double sss;
+        private double pagibig;
+        private double philhealth;
+        private double absences;
+
+        public double SSS
+        {
+            get { return sss; }
+            set { sss = value; }
+        }
+
+        public double PagIbig
+        {
+            get { return pagibig; }
+            set { pagibig = value; }
+        }
+
+        public double PhilHealth
+        {
+            get { return philhealth; }
+            set { philhealth = value; }
+        }
+
+        public double Absences
+        {
+            get { return absences; }
+            set { absences = value; }
+        }
+    }
+}
