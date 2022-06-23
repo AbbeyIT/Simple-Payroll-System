2fe1e93903c76452c2c386c8df785b60495c617d
AbbeyIT <93916892+AbbeyIT@users.noreply.github.com>
2022-06-23 15:33:07 +0800

    add salary information class

---
+++ Salary Computation/SalaryInformation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary_Computation
+{
+    class SalaryInformation
+    {
+        private int employeeID;
+        private double rateInPesos;
+        private int hoursWork;
+        private double salary;
+        private int hoursOT;
+        private double holiPay;
+
+        public int EmployeeID
+        {
+            get { return employeeID; }
+            set { employeeID = value; }
+        }
+
+        public double RateInPesos
+        {
+            get { return rateInPesos; }
+            set { rateInPesos = value; }
+        }
+
+        public int HoursWork
+        {
+            get { return hoursWork; }
+            set { hoursWork = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public int HoursOT
+        {
+            get { return hoursOT; }
+            set { hoursOT = value; }
+        }
+
+        public double HoliPay
+        {
+            get { return holiPay; }
+            set { holiPay = value; }
+        }
+
+    }
+}
